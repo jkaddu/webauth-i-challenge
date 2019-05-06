0a66f10936b70729462264a25f48f083fb09f926
jkaddu <jkaddu@yahoo.com>
2019-05-06 15:40:01 -0700

    updates

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors');
 const bcrypt = require('bcryptjs'); // <<<<<<< added and used for hashing
 
 const db = require('./data/dbConfig.js');
@@ -10,7 +9,6 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
 
 server.post('/api/register', (req, res) => {
 	let user = req.body;
